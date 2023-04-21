3ab10d2966d1c00744699f557240ecb5097ebad5
Salha Siddig <129996035+ssiddig@users.noreply.github.com>
2023-04-21 15:07:59 -0400

    First commit

--- src/main/java/com/ss/Main.java
+++ src/main/java/com/ss/Main.java
@@ -1,7 +1,56 @@
 package com.ss;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
     }
 }
+
+class Book{
+    ///Properties
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean booksCheckedOut;
+    private String checkOutTo;
+
+    ////Constructors
+    public Book (int id, String isbn, String title, boolean booksCheckedOut, String checkOutTo){
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.booksCheckedOut = booksCheckedOut;
+        this.checkOutTo = checkOutTo;
+    }
+
+    ////Setter & Getter
+    public int getId(){
+      return this.id;
+
+    }
+    public void setId(int id){
+        this.id = id;
+
+    }
+    public String getIsbn(){
+        return this.isbn;
+    }
+    public void setIsbn(){
+        this.isbn = isbn;
+
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public String setTitle(){
+        this.title = title;
+    }
+    public Boolean getBooksCheckedOut(){
+        return this.booksCheckedOut;
+    }
+    public Boolean setBooksCheckedOut(){
+        this.booksCheckedOut = booksCheckedOut;
+    }
+
+
+    }
