4204e5c681a36ad4600dacb72614a4bd5eab1a28
Salha Siddig <129996035+ssiddig@users.noreply.github.com>
2023-04-21 16:04:21 -0400

    First commit

--- src/main/java/com/ss/Main.java
+++ src/main/java/com/ss/Main.java
@@ -1,24 +1,36 @@
 package com.ss;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args){
+
+        Scanner myscanner = new Scanner(System.in);
+        Book[] book = new Book[5];
+        book[0] = new Book("The Lorax", 2345643, "43332423" , true, "Sunny");
+        book[1] = new Book("Oh, the Places I'll go", 673862, "73184", false, "Aliea");
+        book[2] = new Book("The 500 Hats", 485839, "293u84", false, "Zara");
+        book[3] = new Book("Green Eggs and Ham", 8347532, "6726284", false, "Kai");
+        book[4] = new Book("The Cat in the Hat", 483924845, "333245", true, "Zayne");
+
+
 
     }
 }
 
 class Book{
     ///Properties
+    private String title;
     private int id;
     private String isbn;
-    private String title;
+
     private boolean booksCheckedOut;
     private String checkOutTo;
 
     ////Constructors
-    public Book (int id, String isbn, String title, boolean booksCheckedOut, String checkOutTo){
+    public Book ( String title, int id, String isbn, boolean booksCheckedOut, String checkOutTo){
+
+        this.title = title;
         this.id = id;
         this.isbn = isbn;
-        this.title = title;
         this.booksCheckedOut = booksCheckedOut;
         this.checkOutTo = checkOutTo;
     }
